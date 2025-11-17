d938354820f647633ed58e5116c6ed894f7dc80d
btd321 <btd321@gmail.com>
2025-11-17 00:21:41 -0500

    created sprite groups

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from logger import log_state
 from player import Player
 
 pygame.init()
-       
+ 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -12,6 +12,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock_obj = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x= SCREEN_WIDTH / 2, y= SCREEN_HEIGHT /2)
 
     while True:
@@ -20,8 +23,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for draws in drawable:
+            draws.draw(screen)
         pygame.display.flip()
         dt = clock_obj.tick(60)/1000
         
